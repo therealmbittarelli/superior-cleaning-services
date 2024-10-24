09cc7d29cdc1e84e475470ab509cdb8cae588f58
therealmbittarelli <55117561+therealmbittarelli@users.noreply.github.com>
2024-10-23 22:21:25 -0400

    Add ContactForm scaffolding

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -7,6 +7,9 @@
 //------------------------------------------------------------------------------
 // Imports
 //------------------------------------------------------------------------------
+// Libraries
+import { useState } from 'react';
+
 // CSS
 import './ContactForm.css';
 
@@ -21,10 +24,47 @@ import './ContactForm.css';
 
 const ContactForm = () => {
 
+  const [ name, setName] = useState('');
+  const [ email, setEmail ] = useState('');
+  const [ message, setMessage ] = useState('');
+
+  const handleChange = (evt) => {
+    evt.preventDefault();
+    console.log('formd', name)
+    if (evt.target === 'name') {
+      setName(evt.target.value);
+    }
+    else if (evt.target === 'email') {
+      setEmail(evt.target.value);
+    }
+    else if (evt.target === 'message') {
+      setMessage(evt.target.value);
+    }
+  };
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    console.log("doop")
+  }
+
   return (
-    <form>
-        <label>hello</label>
-        <input />
+    <form id="contact-form" onSubmit={handleSubmit}>
+      <label htmlFor="name">
+        Name*
+        <input type="text" value={name} id="name" onChange={(evt) => handleChange(evt)} required/>
+      </label>
+
+      <label htmlFor="email">
+        Email*
+        <input type="email" value={email} id="email" onChange={handleChange} required />
+      </label>
+
+      <label htmlFor="message">
+        Message*
+        <textarea id="message" value={message} onChange={handleChange} required/>
+      </label>
+
+      <button type="submit">Submit</button>
     </form>
   )
 }
